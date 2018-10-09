cd5fc0c665e85dd836572cb1ce7747ebbd5a02dc
Srinivasan.G <gopalasr@gmail.com>
2018-10-09 21:33:36 +0530

    added noodles

--- hungry.py
+++ hungry.py
@@ -1,7 +1,8 @@
 hungry = Input("Are you Hungry ?")
 if (hungry == "yes"):
 	print("I am Hungry")
-	print("Eat Pizza")(
+	print("Eat Pizza")
+	print("eat noodles")
 else:
 	thirsty = input("Are you thirsty?")
 	if (thirsty == 'yes'):
